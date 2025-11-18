ff509d3796fba019cb09b0d28e7d26384b58e98e
Breno Trindade <breno.trindade2011@gmail.com>
2025-11-17 23:06:55 -0300

    feat: Implement login and register methods in AuthService

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+
+import { AuthResponse, LoginQuery, RegisterUserCommand } from '../../models/auth.models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  
+  private apiUrl = `${environment.apiUrl}/api/Users`;
+
+  constructor(private http: HttpClient){}
+
+  login(query: LoginQuery): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login`,query);
+  }
+
+  register(command: RegisterUserCommand): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, command);
+  }
+}
