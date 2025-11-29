47c9e2a06747cf20018698e85c56469cd2c8c08e
Breno Trindade <breno.trindade2011@gmail.com>
2025-11-28 22:47:09 -0300

    feat: add logout, profile and settings buttons on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
+import { UserService } from '../../core/services/user.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +12,33 @@ import { AuthService } from '../../core/services/auth.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   isPlaying = false;
+  isDropdownOpen = false;
+  user: User | null = null;
 
   constructor(
     private authService: AuthService,
+    private userService: UserService,
     private router: Router
   ) {}
 
+  ngOnInit(): void {
+    const decodedToken = this.authService.getDecodedToken();
+    if (decodedToken) {
+      const userId = decodedToken.sub;
+      this.userService.getUser(userId).subscribe(user => {
+        this.user = user;
+      });
+    }
+  }
+
   togglePlay() {
     this.isPlaying = !this.isPlaying;
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 
   logout() {
